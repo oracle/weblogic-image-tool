10eb8165a2f0282b9781a61b015e878e8fd388f6
johnny.shum@oracle.com <johnny.shum@oracle.com>
2019-01-10 15:44:50 -0600

    fix various http client access issues with oracle site
    Former-commit-id: 668997084cc87bdb158ac0af46e29b509257cf49

--- src/main/java/com/oracle/weblogicx/imagebuilder/builder/util/HttpUtil.java
+++ src/main/java/com/oracle/weblogicx/imagebuilder/builder/util/HttpUtil.java
@@ -1,75 +1,94 @@
 package com.oracle.weblogicx.imagebuilder.builder.util;
 
-
 import java.io.File;
 import java.io.IOException;
-import java.nio.charset.Charset;
+import java.io.StringReader;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.StatusLine;
 import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpResponseException;
-import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.CookieStore;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.fluent.Executor;
 import org.apache.http.client.fluent.Request;
-import org.apache.http.entity.ContentType;
-import org.apache.http.protocol.HTTP;
+import org.apache.http.impl.client.BasicCookieStore;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.cookie.BasicClientCookie;
 import org.w3c.dom.Document;
+import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 
 public class HttpUtil {
 
-    public static Document getXMLContent(String url) throws IOException {
 
-        Document result = Request.Get(url)
-            .execute().handleResponse(new ResponseHandler<Document>() {
+    public static Document getXMLContent(String url, String username, String password) throws IOException {
 
-                public Document handleResponse(final HttpResponse response) throws IOException {
-                    StatusLine statusLine = response.getStatusLine();
-                    HttpEntity entity = response.getEntity();
-                    if (statusLine.getStatusCode() >= 300) {
-                        throw new HttpResponseException(
-                            statusLine.getStatusCode(),
-                            statusLine.getReasonPhrase());
-                    }
-                    if (entity == null) {
-                        throw new ClientProtocolException("Response contains no content");
-                    }
-                    DocumentBuilderFactory dbfac = DocumentBuilderFactory.newInstance();
-                    try {
-                        DocumentBuilder docBuilder = dbfac.newDocumentBuilder();
-                        ContentType contentType = ContentType.getOrDefault(entity);
-                        if (!contentType.equals(ContentType.APPLICATION_XML)) {
-                            throw new ClientProtocolException("Unexpected content type:" +
-                                contentType);
-                        }
-                        String charset =  contentType.getCharset().toString();
-                        if (charset == null) {
-                            charset = HTTP.DEFAULT_CONTENT_CHARSET;
-                        }
-                        return docBuilder.parse(entity.getContent(), charset);
-                    } catch (ParserConfigurationException ex) {
-                        throw new IllegalStateException(ex);
-                    } catch (SAXException ex) {
-                        throw new ClientProtocolException("Malformed XML document", ex);
-                    }
-                }
+        RequestConfig.Builder config = RequestConfig.custom();
+        config.setCircularRedirectsAllowed(true);
 
-            });
 
-        return result;
+        CookieStore cookieStore = new BasicCookieStore();
+        BasicClientCookie cc = new BasicClientCookie("oraclelicense", "a");
+        cc.setDomain("edelivery.oracle.com");
+        cookieStore.addCookie(cc);
+
+        CloseableHttpClient client = HttpClientBuilder.create()
+            .setDefaultRequestConfig(config.build())
+            .useSystemProperties()
+            .build();
+
+        Executor httpExecutor = Executor.newInstance(client).auth(username, password);
+        httpExecutor.use(cookieStore);
+
+        String xmlString = httpExecutor.execute(Request.Get(url)
+            .connectTimeout(30000)
+            .socketTimeout(30000)).returnContent().asString();
+
+        try {
+            DocumentBuilderFactory dbfac = DocumentBuilderFactory.newInstance();
+            DocumentBuilder docBuilder = dbfac.newDocumentBuilder();
+            InputSource is = new InputSource(new StringReader(xmlString));
+            return docBuilder.parse(is);
+        } catch (ParserConfigurationException ex) {
+            throw new IllegalStateException(ex);
+        } catch (SAXException ex) {
+            throw new ClientProtocolException("Malformed XML document", ex);
+        }
 
     }
 
-    public static void downloadFile(String url, String destination) throws IOException {
-        Request.Get(url)
-            .connectTimeout(1000)
-            .socketTimeout(1000)
-            .execute().saveContent(new File(destination));
+    public static void downloadFile(String url, String destination, String username, String password) throws
+        IOException {
+        RequestConfig.Builder config = RequestConfig.custom();
+        config.setCircularRedirectsAllowed(true);
+        //        config.setCookieSpec(CookieSpecs.BROWSER_COMPATIBILITY);
+
+
+        CookieStore cookieStore = new BasicCookieStore();
+        BasicClientCookie cc = new BasicClientCookie("oraclelicense", "a");
+        cc.setDomain("edelivery.oracle.com");
+        cookieStore.addCookie(cc);
+
+        CloseableHttpClient client = HttpClientBuilder.create()
+            .setDefaultRequestConfig(config.build())
+            .useSystemProperties()
+            .build();
+
+        Executor httpExecutor = Executor.newInstance(client).auth(username, password);
+        httpExecutor.use(cookieStore);
+
+        httpExecutor.execute(Request.Get(url)
+            .connectTimeout(30000)
+            .socketTimeout(30000)).saveContent(new File(destination));
+
     }
 
+
+    public static void main(String args[]) throws IOException {
+
+
+    }
 }
