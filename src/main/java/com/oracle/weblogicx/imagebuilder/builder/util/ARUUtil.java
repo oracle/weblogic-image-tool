9e54b7ffa99ebbabb71a0cf874b6d358c7d771c4
johnny.shum@oracle.com <johnny.shum@oracle.com>
2019-01-10 17:36:54 -0600

    add check conflicts method
    Former-commit-id: 7770967756250b7536a3ad5e9688841f32d9bbeb

--- src/main/java/com/oracle/weblogicx/imagebuilder/builder/util/ARUUtil.java
+++ src/main/java/com/oracle/weblogicx/imagebuilder/builder/util/ARUUtil.java
@@ -1,6 +1,7 @@
 package com.oracle.weblogicx.imagebuilder.builder.util;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -9,7 +10,6 @@ import javax.xml.xpath.XPathExpressionException;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
-import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
 public class ARUUtil {
@@ -19,8 +19,37 @@ public class ARUUtil {
         getAllReleases("wls", userId, password);
     }
 
-    public void getLatestWLSPatches(String version, String userId, String password) throws IOException {
-        getLatestReleases("wls", version, userId, password);
+    public void getLatestWLSPatches(String release, String userId, String password) throws IOException {
+        getLatestPSU("wls", release, userId, password);
+    }
+
+    public void getPatches(String category, List<String> patches, String userId, String password) throws IOException {
+        for (String patch : patches)
+            getPatch("wls", patch, userId, password);
+    }
+
+    public void validatePatches(List<String> patches, String category, String version) {
+
+        // TODO
+
+        // find the release number first based on the version
+        // build the xml
+
+//        <conflict_check_request>
+//  <platform>912</platform>
+//  <target_patch_list>
+//    <installed_patch/>
+//  </target_patch_list>
+//  <candidate_patch_list>
+//    <patch_group rel_id="80111060" language_id="0">7044721</patch_group>
+//    <patch_group rel_id="80111060" language_id="0">7156923</patch_group>
+//    <patch_group rel_id="80111060" language_id="0">7210195</patch_group>
+//    <patch_group rel_id="80111060" language_id="0">7256747</patch_group>
+//  </candidate_patch_list>
+//</conflict_check_request>
+
+//   Run against POST  /Orion/Services/conflict_checks
+
     }
 
     private Document getAllReleases(String category, String userId, String password) throws IOException {
@@ -29,7 +58,7 @@ public class ARUUtil {
        // "https://updates.oracle.com/Orion/Services/metadata?table=aru_releases" -o allarus.xml)
 
         Document allReleases = HttpUtil.getXMLContent("https://updates.oracle"
-            + ".com/Orion/Services/metadata?table=aru_releases");
+            + ".com/Orion/Services/metadata?table=aru_releases", userId, password);
 
         try {
 
@@ -62,31 +91,60 @@ public class ARUUtil {
 
     }
 
-    private void getLatestReleases(String category, String version, String userId, String password) throws IOException {
+    private void getLatestPSU(String category, String release, String userId, String password) throws IOException {
 
 //        HTTP_STATUS=$(curl -v -w "%{http_code}" -b cookies.txt -L --header 'Authorization: Basic ${basicauth}' "https://updates.oracle.com/Orion/Services/search?product=15991&release=$releaseid&include_prereqs=true" -o latestpsu.xml)
 
 
         Document allPatches = HttpUtil.getXMLContent("https://updates.oracle"
-            + ".com/Orion/Services/search?product=15991&release=" + version);
+            + ".com/Orion/Services/search?product=15991&release=" + release, userId, password);
 
+        savepatch(allPatches, userId, password);
+    }
+
+    private void getPatch(String category, String patchNumber, String userId, String password) throws IOException {
+
+        //        HTTP_STATUS=$(curl -v -w "%{http_code}" -b cookies.txt -L --header 'Authorization: Basic ${basicauth}' "https://updates.oracle.com/Orion/Services/search?product=15991&release=$releaseid&include_prereqs=true" -o latestpsu.xml)
+
+
+        Document allPatches = HttpUtil.getXMLContent("https://updates.oracle"
+            + ".com/Orion/Services/search?product=15991&bug=" + patchNumber, userId, password);
+
+        savepatch(allPatches, userId, password);
+
+
+
+    }
+
+    private void savepatch(Document allPatches, String userId, String password) throws IOException {
         try {
+
+            // TODO: needs to make sure there is one and some filtering if not sorting
+
             String downLoadLink = XPathUtil.applyXPathReturnString(allPatches, "string"
                 + "(/results/patch[1]/files/file/download_url/text())");
 
             String doloadHost = XPathUtil.applyXPathReturnString(allPatches, "string"
                 + "(/results/patch[1]/files/file/download_url/@host)");
 
+            String bugname  = XPathUtil.applyXPathReturnString(allPatches, "string"
+                + "(/results/patch[1]/name");
 
-            HttpUtil.downloadFile(doloadHost+downLoadLink, "todo");
+            // TODO find the download location
+
+            String fileName = bugname + ".zip";
+
+            HttpUtil.downloadFile(doloadHost+downLoadLink, fileName, userId, password);
+
+            // TODO need method to update the cache data table ?
 
         } catch (XPathExpressionException xpe) {
             throw new IOException(xpe);
         }
 
-
-
     }
+
+
 
 }
 
