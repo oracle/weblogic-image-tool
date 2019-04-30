06e7677d22accb9ebe1db284ed0553e5147f8e08
Dave Diamond <dave.diamond@oracle.com>
2019-04-30 15:09:29 +0000

    Fix the setup; also add a file to gitignore

--- src/main/bin/setup.sh
+++ src/main/bin/setup.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+# source setup.sh
 umask 27
 #
 # Make sure that the JAVA_HOME environment variable is set to point to a
@@ -6,24 +6,24 @@ umask 27
 #
 if [ "${JAVA_HOME}" = "" ]; then
   echo "Please set the JAVA_HOME environment variable to point to a Java 8 installation" >&2
-  exit 2
+  return
 elif [ ! -d "${JAVA_HOME}" ]; then
   echo "Your JAVA_HOME environment variable to points to a non-existent directory: ${JAVA_HOME}" >&2
-  exit 2
+  return
 fi
 
 if [ -x "${JAVA_HOME}/bin/java" ]; then
   JAVA_EXE=${JAVA_HOME}/bin/java
 else
   echo "Java executable at ${JAVA_HOME}/bin/java either does not exist or is not executable" >&2
-  exit 2
+  return
 fi
 
 JVM_OUTPUT=`${JAVA_EXE} -version 2>&1`
 case "${JVM_OUTPUT}" in
   *OpenJDK*)
     echo "JAVA_HOME ${JAVA_HOME} contains OpenJDK, which is not supported" >&2
-    exit 2
+    return
     ;;
 esac
 
@@ -45,7 +45,7 @@ echo $RESULT_PATH
 }
 
 unalias imagebuilder 2> /dev/null
-script_dir=$( dirname "$( read_link "${BASH_SOURCE[-1]}" )" )
+script_dir=$( dirname "$( read_link "${BASH_SOURCE[0]}" )" )
 IMAGEBUILDER_HOME=`cd "${script_dir}/.." ; pwd`
 export IMAGEBUILDER_HOME
 alias imagebuilder="${JAVA_HOME}/bin/java -cp \"${IMAGEBUILDER_HOME}/lib/*\" com.oracle.weblogicx.imagebuilder.cli.CLIDriver"
