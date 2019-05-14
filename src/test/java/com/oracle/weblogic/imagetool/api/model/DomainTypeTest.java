c9a7240e9f6d79324ceaf0028320f70555528ce1
Derek Sharpe <derek.sharpe@oracle.com>
2019-05-14 14:08:53 -0500

    added junit dummy test

---
+++ src/test/java/com/oracle/weblogic/imagetool/api/model/DomainTypeTest.java
@@ -0,0 +1,13 @@
+package com.oracle.weblogic.imagetool.api.model;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DomainTypeTest {
+
+    @org.junit.Test
+    public void fromValue() {
+        assertEquals("Dummy", true, true);
+    }
+}
