b4df7fec94bb7130307dc20dbb80157e6abe2e0e
Derek Sharpe <derek.sharpe@oracle.com>
2019-08-30 10:30:38 -0500

    added optional unix script to run imagetool

--- imagetool/src/main/bin/imagetool.sh
+++ imagetool/src/main/bin/imagetool.sh
@@ -3,32 +3,34 @@
 #
 #Licensed under the Universal Permissive License v 1.0 as shown at http://oss.oracle.com/licenses/upl.
 #
-# source setup.sh
-umask 27
+# imagetool.sh
+
+set -e
+
 #
 # Make sure that the JAVA_HOME environment variable is set to point to a
 # JDK 8 or higher JVM (and that it isn't OpenJDK).
 #
 if [ -z "${JAVA_HOME}" ]; then
   echo "Please set the JAVA_HOME environment variable to match the location of your Java 8 installation." >&2
-  return
+  exit -1
 elif [ ! -d "${JAVA_HOME}" ]; then
   echo "Your JAVA_HOME environment variable points to a non-existent directory: ${JAVA_HOME}" >&2
-  return
+  exit -1
 fi
 
 if [ -x "${JAVA_HOME}/bin/java" ]; then
   JAVA_EXE=${JAVA_HOME}/bin/java
 else
   echo "Java executable at ${JAVA_HOME}/bin/java either does not exist or is not executable" >&2
-  return
+  exit -1
 fi
 
 JVM_OUTPUT=`${JAVA_EXE} -version 2>&1`
 case "${JVM_OUTPUT}" in
   *OpenJDK*)
     echo "JAVA_HOME ${JAVA_HOME} contains OpenJDK, which is not supported" >&2
-    return
+    exit -1
     ;;
 esac
 
@@ -49,9 +51,8 @@ cd $PREV_DIR
 echo $RESULT_PATH
 }
 
-unalias imagetool 2> /dev/null
 script_dir=$( dirname "$( read_link "${BASH_SOURCE[0]}" )" )
 IMAGETOOL_HOME=`cd "${script_dir}/.." ; pwd`
 export IMAGETOOL_HOME
-alias imagetool="${JAVA_HOME}/bin/java -cp \"${IMAGETOOL_HOME}/lib/*\" -Djava.util.logging.config.file=${IMAGETOOL_HOME}/bin/logging.properties com.oracle.weblogic.imagetool.cli.CLIDriver"
-source ${IMAGETOOL_HOME}/lib/imagetool_completion.sh
+${JAVA_HOME}/bin/java -cp "${IMAGETOOL_HOME}/lib/*" -Djava.util.logging.config.file=${IMAGETOOL_HOME}/bin/logging.properties com.oracle.weblogic.imagetool.cli.CLIDriver $@
+
