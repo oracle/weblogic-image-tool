9b2e42e16cb9292198639fa968ad828689356030
xiancao <xian.cao@oracle.com>
2019-10-10 17:36:12 +0000

    clean up db container

--- imagetool/src/test/java/com/oracle/weblogic/imagetool/integration/BaseTest.java
+++ imagetool/src/test/java/com/oracle/weblogic/imagetool/integration/BaseTest.java
@@ -94,13 +94,11 @@ public class BaseTest {
         command = "mkdir " + wlsImgCacheDir;
         executeNoVerify(command);
 
-        // clean up the docker images
-        command = "docker stop " + dbContainerName;
+        // clean up the db container
+        command = "docker rm -f " + dbContainerName;
         executeNoVerify(command);
-        // command = "docker rmi -f " + BASE_OS_IMG + ":" + BASE_OS_IMG_TAG + " " + ORACLE_DB_IMG + ":"
-        //     + ORACLE_DB_IMG_TAG;
-        // executeNoVerify(command);
 
+        // clean up the images created in the tests
         command = "docker rmi -f $(docker images | grep " + build_tag + " | tr -s ' ' | cut -d ' ' -f 3)";
         executeNoVerify(command);
 
