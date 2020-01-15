036d6683eddf998f832b32f3e952f3950b4ec05b
Derek Sharpe <derek.sharpe@oracle.com>
2020-01-15 15:47:20 -0600

    capitalize constant string name

--- imagetool/src/main/java/com/oracle/weblogic/imagetool/cli/menu/CommonOptions.java
+++ imagetool/src/main/java/com/oracle/weblogic/imagetool/cli/menu/CommonOptions.java
@@ -73,13 +73,13 @@ public abstract class CommonOptions {
         }
 
         if (additionalBuildFiles != null) {
-            final String filesDir = "files";
-            Files.createDirectory(Paths.get(getTempDirectory(), filesDir));
+            final String FILES_DIR = "files";
+            Files.createDirectory(Paths.get(getTempDirectory(), FILES_DIR));
             for (Path additionalFile : additionalBuildFiles) {
                 if (!Files.isRegularFile(additionalFile)) {
                     throw new FileNotFoundException(Utils.getMessage("IMG-0030", additionalFile));
                 }
-                Path targetFile = Paths.get(getTempDirectory(), filesDir, additionalFile.getFileName().toString());
+                Path targetFile = Paths.get(getTempDirectory(), FILES_DIR, additionalFile.getFileName().toString());
                 logger.info("IMG-0043", additionalFile);
                 Utils.copyLocalFile(additionalFile.toString(), targetFile.toString(), false);
             }
