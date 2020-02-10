7544bfcf4a1376bacbfb0947f151f93514a517fc
Derek Sharpe <36005286+ddsharpe@users.noreply.github.com>
2020-02-10 10:29:42 -0600

    removed All rights reserved from copyright (#137)

--- imagetool/src/main/resources/probe-env/test-create-env.sh
+++ imagetool/src/main/resources/probe-env/test-create-env.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #
-#Copyright (c) 2019, Oracle and/or its affiliates.  All rights reserved.
+#Copyright (c) 2019, 2020, Oracle and/or its affiliates.
 #
 #Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 #
