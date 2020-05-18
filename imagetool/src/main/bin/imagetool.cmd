66dec5e6afb28370b6a745c61fdc00ff01a1d456
Derek Sharpe <36005286+ddsharpe@users.noreply.github.com>
2020-05-18 16:25:51 -0500

    match text to other shell scripts for JAVA_HOME

--- imagetool/src/main/bin/imagetool.cmd
+++ imagetool/src/main/bin/imagetool.cmd
@@ -6,7 +6,7 @@
 @rem Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 
 IF "%JAVA_HOME%" == "" (
-  ECHO JAVA_HOME not set. Please set the JAVA_HOME environment variable to match the location of your Java 8 or 11 installation.
+  ECHO Please set the JAVA_HOME environment variable to match the location of your Java installation. Java 8 or newer is required.
   EXIT /B 2
 )
 FOR %%i IN ("%JAVA_HOME%") DO SET JAVA_HOME=%%~fsi
