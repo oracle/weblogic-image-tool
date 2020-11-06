f5c085bb43b1e1ab97bba03fe6d5b48040ab7088
Derek Sharpe <derek.sharpe@oracle.com>
2020-11-05 19:09:02 -0600

    added support for adding additional OS packages to AdditionalBuildCommands

--- imagetool/src/main/java/com/oracle/weblogic/imagetool/util/AdditionalBuildCommands.java
+++ imagetool/src/main/java/com/oracle/weblogic/imagetool/util/AdditionalBuildCommands.java
@@ -21,6 +21,7 @@ import com.oracle.weblogic.imagetool.logging.LoggingFactory;
 public class AdditionalBuildCommands {
     private static final LoggingFacade logger = LoggingFactory.getLogger(AdditionalBuildCommands.class);
 
+    public static final String PACKAGES = "package-manager-packages";
     public static final String BEFORE_JDK = "before-jdk-install";
     public static final String AFTER_JDK = "after-jdk-install";
     public static final String BEFORE_FMW = "before-fmw-install";
@@ -34,6 +35,7 @@ public class AdditionalBuildCommands {
 
     private AdditionalBuildCommands() {
         sections = new ArrayList<>();
+        sections.add(getPattern(PACKAGES));
         sections.add(getPattern(BEFORE_JDK));
         sections.add(getPattern(AFTER_JDK));
         sections.add(getPattern(BEFORE_FMW));
