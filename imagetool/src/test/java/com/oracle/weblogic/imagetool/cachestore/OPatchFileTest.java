82b1b1efbba1d1d5599fbaf3c15f45d23f93c24b
Derek Sharpe <derek.sharpe@oracle.com>
2020-11-17 13:08:53 -0600

    added a couple negative test cases

--- imagetool/src/test/java/com/oracle/weblogic/imagetool/cachestore/OPatchFileTest.java
+++ imagetool/src/test/java/com/oracle/weblogic/imagetool/cachestore/OPatchFileTest.java
@@ -6,6 +6,7 @@ package com.oracle.weblogic.imagetool.cachestore;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @Tag("unit")
@@ -14,5 +15,9 @@ public class OPatchFileTest {
     void opatchPatchIdTest() {
         assertTrue(OPatchFile.isOPatchPatch("28186730"), "OPatch bug number");
         assertTrue(OPatchFile.isOPatchPatch("28186730_13.9.4.2.4"), "OPatch bug number with version");
+        assertTrue(OPatchFile.isOPatchPatch("28186730_1"), "OPatch bug number with separator");
+
+        assertFalse(OPatchFile.isOPatchPatch("28186731"), "Should not match");
+        assertFalse(OPatchFile.isOPatchPatch("281867301"), "OPatch bug number with additional number");
     }
 }
