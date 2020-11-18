1c62a806be68e96022f167b382358d1199796e37
Johnny Shum <johnny.shum@oracle.com>
2020-11-17 18:19:41 -0600

    Try to avoid copy issue with certain version of docker (#220)

--- imagetool/src/main/java/com/oracle/weblogic/imagetool/util/DockerfileOptions.java
+++ imagetool/src/main/java/com/oracle/weblogic/imagetool/util/DockerfileOptions.java
@@ -314,6 +314,14 @@ public class DockerfileOptions {
         return wdtModelHome;
     }
 
+    /**
+     * Check to see if WDT model home is not under WDT home.
+     * @return true|false
+     */
+    public boolean isWdtModelHomeOutsideWdtHome() {
+        return !wdtModelHome.startsWith(wdtHome + File.separator);
+    }
+
     public boolean isWdtValidateEnabled() {
         return isWdtEnabled() && modelOnly() && (!wdtModelList.isEmpty() || !wdtArchiveList.isEmpty());
     }
