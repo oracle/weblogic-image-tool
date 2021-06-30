81902cdc7f631ea68f839808d4a8318f04e7f607
Derek Sharpe <derek.sharpe@oracle.com>
2021-06-29 20:01:27 -0500

    FMWPLAT was removed from WLS 14+

---
+++ imagetool/src/main/java/com/oracle/weblogic/imagetool/aru/ReleaseNotFoundException.java
@@ -0,0 +1,16 @@
+// Copyright (c) 2021, Oracle and/or its affiliates.
+// Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+
+package com.oracle.weblogic.imagetool.aru;
+
+/**
+ * For a given list of patches returned from ARU, the version requested was not found.
+ */
+public class ReleaseNotFoundException extends AruException {
+    /**
+     * The product/version combination was not found in the release table on ARU.
+     */
+    public ReleaseNotFoundException() {
+        super();
+    }
+}
