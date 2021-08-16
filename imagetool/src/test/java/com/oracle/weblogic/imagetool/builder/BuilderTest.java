119436711fdf912278d89dc7a3e7c5c33341bc43
Derek Sharpe <derek.sharpe@oracle.com>
2021-08-16 12:56:35 -0500

    add additional unit tests

---
+++ imagetool/src/test/java/com/oracle/weblogic/imagetool/builder/BuilderTest.java
@@ -0,0 +1,46 @@
+// Copyright (c) 2021, Oracle and/or its affiliates.
+// Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+
+package com.oracle.weblogic.imagetool.builder;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@Tag("unit")
+class BuilderTest {
+    private final String buildContext = "/some/context";
+    private final String buildEngine = "docker";
+
+    private String expected(String options) {
+        return String.format("%s build --no-cache %s %s", buildEngine, options, buildContext);
+    }
+
+    @Test
+    void testSimpleCommand() {
+        BuildCommand cmd = new BuildCommand(buildEngine, buildContext);
+        cmd.tag("img:1");
+        assertEquals(expected("--tag img:1"), cmd.toString());
+    }
+
+    @Test
+    void testBuildWithOptions() {
+        BuildCommand cmd = new BuildCommand(buildEngine, buildContext)
+            .tag("img:2")
+            .pull(true)
+            .forceRm(true)
+            .network("private-net")
+            .buildArg("http_proxy", "http://blah/blah");
+        assertEquals(
+            expected("--tag img:2 --pull --force-rm --network private-net --build-arg http_proxy=http://blah/blah"),
+            cmd.toString());
+    }
+
+    @Test
+    void testBuildWithProxyPass() {
+        BuildCommand cmd = new BuildCommand(buildEngine, buildContext);
+        cmd.tag("img:3").buildArg("http_proxy", "http://user:pass@blah/blah", true);
+        assertEquals(expected("--tag img:3 --build-arg http_proxy=********"), cmd.toString());
+    }
+}
