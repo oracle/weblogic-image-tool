1d4d2697dd200065c0e3eac6a86fc94289495cb2
Derek Sharpe <derek.sharpe@oracle.com>
2022-04-22 19:39:08 -0500

    added new unit test for FileFormatter

--- imagetool/src/test/java/com/oracle/weblogic/imagetool/logging/FileFormatterTest.java
+++ imagetool/src/test/java/com/oracle/weblogic/imagetool/logging/FileFormatterTest.java
@@ -3,15 +3,34 @@
 
 package com.oracle.weblogic.imagetool.logging;
 
+import java.util.logging.Level;
+import java.util.logging.LogRecord;
+
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 class FileFormatterTest {
 
     @Test
     void format() {
-        assertEquals(true, true);
+        FileFormatter formatter = new FileFormatter();
+        LogRecord logRecord = new LogRecord(Level.INFO, "message goes here");
+
+        String logMessage = formatter.format(logRecord);
+        assertTrue(logMessage.startsWith("####"));
+        assertTrue(logMessage.endsWith("<message goes here>\n"));
+    }
+
+    @Test
+    void formatWithColor() {
+        FileFormatter formatter = new FileFormatter();
+        String initialValue = "message [[brightred: goes]] here";
+        LogRecord logRecord = new LogRecord(Level.INFO, initialValue);
+
+        String logMessage = formatter.format(logRecord);
+        // color should be removed for non-console logging
+        assertTrue(logMessage.endsWith("<message goes here>\n"));
     }
 }
